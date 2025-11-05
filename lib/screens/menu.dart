a5b37c6848234a2b837a6352364e249687fbaa4c
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-11-05 14:27:44 +0700

    Tutorial 7 complete: Refactoring

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:football_news/widgets/left_drawer.dart';
-import 'package:football_news/newslist_form.dart';
+import 'package:football_news/widgets/news_card.dart';
 
 /*
 class MyHomePage extends StatefulWidget {
@@ -154,66 +154,7 @@ class ItemHomepage {
 
 }
 
-class ItemCard extends StatelessWidget {
 
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Menentukan warna latar belakang dari tema aplikasi.
-      color: Theme.of(context).colorScheme.secondary,
-      // Membuat sudut kartu melengkung.
-      borderRadius: BorderRadius.circular(12),
-
-      child: InkWell(
-        // Aksi ketika kartu ditekan.
-        onTap: () {
-          // Menampilkan pesan SnackBar saat kartu ditekan.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-                SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
-          // Navigate ke route yang sesuai (tergantung jenis tombol)
-          if (item.name == "Add News") {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => NewsFormPage(),
-              ));
-
-          }
-        },
-        // Container untuk menyimpan Icon dan Text
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              // Menyusun ikon dan teks di tengah kartu.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 /*
 class _MyHomePageState extends State<MyHomePage> {
